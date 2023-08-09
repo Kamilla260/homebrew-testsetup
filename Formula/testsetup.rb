0beb59e7678b2cfff1fd41c9715910493b79ffa3
Kamilla Lukaszewska <kamilla.lukaszewska@affirm.com>
2023-08-09 10:47:58 +0200

    v

--- Formula/testsetup.rb
+++ Formula/testsetup.rb
@@ -2,18 +2,21 @@ class Testsetup < Formula
   include Language::Python::Virtualenv
   desc "First formulea for tests"
   homepage "https://froggytesto.jfrog.io/"
-  url "https://froggytesto.jfrog.io/artifactory/testsetup/testsetup-1.1.5.tar.gz"
-  sha256 "7a50ede8ab17f15af84c9ea41684b01a3bb6fd0e31b722677ccf7cd92e156c3e"
-
-  depends_on "python@3.8"
-
-  resource "httptools" do
-    url "https://files.pythonhosted.org/packages/d9/6f/aad92c3f090e2f74dd728d58d3bba4c832d35199814af99673ee7300b582/httptools-0.1.1.tar.gz"
-    sha256 "41b573cf33f64a8f8f3400d0a7faf48e1888582b6f6e02b82b9bd4f0bf7497ce"
+  url "https://frogets.jfrog.io/artifactory/generic-local/testsetup-1.1.5.zip"
+  sha256 "270298b1ae40d6905fdee0e5e783213a919f34f9a0f444ac9e2935ca3cd56f11"
+  depends_on "python-typing-extensions"
+  depends_on "python@3.11"
+  depends_on "pyyaml"
+  depends_on "six"
+​
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/28/99/2dfd53fd55ce9838e6ff2d4dac20ce58263798bd1a0dbe18b3a9af3fcfce/anyio-3.7.1.tar.gz"
+    sha256 "44a3c9aba0f5defa43261a8b3efb97891f2bd7d804e0e1f56419befa1adfc780"
   end
-
+​
+​
   def install
     virtualenv_install_with_resources
   end
-
+​
 end
